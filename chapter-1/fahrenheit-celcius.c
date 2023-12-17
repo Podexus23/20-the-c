561215ccda8adfd4fdf7f86189feb2dd030840bb
Podexus23 <santon23@gmail.com>
2023-12-17 14:07:15 +0300

    feat: exercise 1.5

--- chapter-1/fahrenheit-celcius.c
+++ chapter-1/fahrenheit-celcius.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celcius table for fahr = 0 , 20, ..., 300; */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 int main()
 {
-  float fahr, celcius;
-  int lower, upper, step;
+  float fahr;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
   printf("Fahrenheit-Celcius table\n");
-  while (fahr <= upper)
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
   {
-    celcius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%3.0f %6.1f\n", fahr, celcius);
-    fahr = fahr + step;
+    printf("%3.0f %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
   }
 }
