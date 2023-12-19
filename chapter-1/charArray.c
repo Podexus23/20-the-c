34362e945c6e9923f0fd5bbd94c82784de8e708f
Podexus23 <santon23@gmail.com>
2023-12-19 16:37:56 +0300

    feat: just count max length of the array

--- chapter-1/charArray.c
+++ chapter-1/charArray.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#define MAXLINE 1000 /* maximum input line size */
+#define MAXLINE 20 /* maximum input line size */
 
 int getLine(char line[], int maxline);
 void copy(char to[], char from[]);
 
+// делаем ещё один запасной массив
+//  если длина строки больше него, пишем сообщение что строка слишком длинная для вывода целиком, но вот длинна её вот такая
+
 /* print longest input line */
 int main()
 {
+  int maxlineNum;
+  maxlineNum = MAXLINE;
+
   int len;               /* current line length */
   int max;               /* maximum length seen so far */
   char line[MAXLINE];    /* current input line */
@@ -23,7 +29,11 @@ int main()
   }
   if (max > 0)
   {
-    printf("%s", longest); /* there was a line */
+    printf("%s", longest);
+    // printf('Line is too long to copy, the limit is:');
+
+    printf("\n");                      /* there was a line */
+    printf("line length: %d \n", max); /* there was a line */
   }
   return 0;
 }
@@ -43,6 +53,10 @@ int getLine(char s[], int lim)
     ++i;
   }
   s[i] = '\0';
+
+  while ((c = getchar()) != EOF && c != '\n')
+    ++i;
+
   return i;
 }
 
