76b13d3e64ffe927893254ae8282cc34102cdf6f
Podexus23 <santon23@gmail.com>
2023-12-22 23:56:00 +0300

    fix: exercie 2.1

--- chapter-2/types.c
+++ chapter-2/types.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
-  printf("signed char min: %d", SCHAR_MIN);
-  printf("signed char max: %d", SCHAR_MAX);
-  printf("unsigned char: %d", UCHAR_MAX);
+  printf("signed char min: %d \n", SCHAR_MIN);
+  printf("signed char max: %d \n", SCHAR_MAX);
+  printf("unsigned char: %d \n", UCHAR_MAX);
   printf("\n");
-  printf("signed short int min: %d", SHRT_MIN);
-  printf("signed short int max: %d", SHRT_MAX);
-  printf("ussigned short int: %d", USHRT_MAX);
+  printf("signed short int min: %d \n", SHRT_MIN);
+  printf("signed short int max: %d \n", SHRT_MAX);
+  printf("ussigned short int: %d \n", USHRT_MAX);
   printf("\n");
-  return 0
+  return 0;
 }
