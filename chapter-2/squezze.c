ff8f703e7d8e37a8642f1738013bd0915633ec66
Podexus23 <santon23@gmail.com>
2023-12-25 18:48:00 +0300

    feat: exrcise 2.4

--- chapter-2/squezze.c
+++ chapter-2/squezze.c
@@ -3,26 +3,25 @@
 #define MAXSIZE 100
 
 int getLine(char line[], int line_length);
+void squeeze(char to_squeeze[], char from[]);
 
 int main()
 {
   char s1[MAXSIZE], s2[MAXSIZE];
-  int lengthS1, lengthS2;
-  lengthS1 = getLine(s1, MAXSIZE);
-  lengthS2 = getLine(s2, MAXSIZE);
 
-  printf("%s\n", s1);
-  printf("%s\n", s2);
+  getLine(s1, MAXSIZE);
+  getLine(s2, MAXSIZE);
+
   squeeze(s1, s2);
-
+  printf("squeezed: %s", s1);
   return 0;
 }
 
 int getLine(char line[], int line_length)
 {
-  int c;
+  int c, i;
 
-  for (int i = 0; i < MAXSIZE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+  for (i = 0; i < line_length - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
   {
     line[i] = c;
   }
@@ -30,6 +29,39 @@ int getLine(char line[], int line_length)
   return i;
 }
 
-void squeeze(char to_delete[], char from_delete[])
+void squeeze(char to_squeeze[], char from[])
 {
+  char copy[MAXSIZE];
+  int i = 0;
+
+  while ((copy[i] = to_squeeze[i]) != '\0')
+  {
+    i++;
+  }
+
+  for (i = 0; from[i] != '\0'; ++i)
+  {
+    int j = 0;
+    while (copy[j] != '\0')
+    {
+      if (copy[j] != from[i])
+      {
+        j++;
+      }
+      else
+      {
+        int t = j;
+        while ((copy[t] = copy[t + 1]) != '\0')
+        {
+          t++;
+        }
+      }
+    }
+  }
+
+  i = 0;
+  while ((to_squeeze[i] = copy[i]) != '\0')
+  {
+    i++;
+  }
 }
