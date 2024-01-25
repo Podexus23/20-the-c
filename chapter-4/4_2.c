324244a9fcf3acd27949c4aeb559e4a570fd9372
Podexus23 <santon23@gmail.com>
2024-01-25 23:59:27 +0300

    feat: 4.2 finally)

--- chapter-4/4_2.c
+++ chapter-4/4_2.c
@@ -21,8 +21,8 @@ int main()
 
 double atof(char s[])
 {
-  double val, power;
-  int i, sign;
+  double val, power, expPower;
+  int i, sign, expSign;
 
   for (i = 0; isspace(s[i]); i++)
     ;
@@ -45,26 +45,40 @@ double atof(char s[])
     val = 10.0 * val + (s[i] - '0');
     power *= 10;
   }
+  printf("some index: %c\n", s[i]);
+  if (s[i] == 'e' || s[i] == 'E')
+  {
+    i++;
+    if (s[i] == '+' || s[i] == '-')
+    {
+      expSign = (s[i] == '-') ? -1 : 1;
+      i++;
+    }
+    else
+    {
+      expSign = 1;
+    }
 
+    for (expPower = 0.0; isdigit(s[i]); i++)
+    {
+      expPower = 10.0 * expPower + (s[i] - '0');
+    }
+
+    {
+      for (; expPower > 0; expPower--)
+      {
+        if (expSign == -1)
+        {
+          power *= 10;
+        }
+        else
+        {
+          power /= 10;
+        }
+      }
+    }
+  }
   return sign * val / power;
-}
-
-int getLine(char str[], int lim)
-{
-  int c, i;
-
-  i = 0;
-  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-  {
-    str[i++] = c;
-  }
-  if (c == '\n')
-  {
-    str[i++] = c;
-  }
-  str[i] = '\0';
-
-  return i;
 }
 
 // int atoi(char s[])
--- helpers/helpers.c
+++ helpers/helpers.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <ctype.h>
 
-#define MAXLINE 10
+#define MAXLINE 15
 
 int getLine(char str[], int lim);
+double atof(char str[]);
 
 int main()
 {
@@ -10,10 +12,70 @@ int main()
   int length;
 
   length = getLine(line, MAXLINE);
-  printf("line: %s", line);
-  printf("line length: %d", length);
-  printf("line unown %d", line[8]);
+  printf("number: %f\n", atof(line));
   return 0;
+}
+
+double atof(char s[])
+{
+  double val, power, expPower;
+  int i, sign, expSign;
+
+  for (i = 0; isspace(s[i]); i++)
+    ;
+
+  sign = (s[i] == '-') ? -1 : 1;
+
+  if (s[i] == '+' || s[i] == '-')
+    i++;
+
+  for (val = 0.0; isdigit(s[i]); i++)
+  {
+    val = 10.0 * val + (s[i] - '0');
+  }
+
+  if (s[i] == '.')
+    i++;
+
+  for (power = 1.0; isdigit(s[i]); i++)
+  {
+    val = 10.0 * val + (s[i] - '0');
+    power *= 10;
+  }
+  printf("some index: %c\n", s[i]);
+  if (s[i] == 'e' || s[i] == 'E')
+  {
+    i++;
+    if (s[i] == '+' || s[i] == '-')
+    {
+      expSign = (s[i] == '-') ? -1 : 1;
+      i++;
+    }
+    else
+    {
+      expSign = 1;
+    }
+
+    for (expPower = 0.0; isdigit(s[i]); i++)
+    {
+      expPower = 10.0 * expPower + (s[i] - '0');
+    }
+
+    {
+      for (; expPower > 0; expPower--)
+      {
+        if (expSign == -1)
+        {
+          power *= 10;
+        }
+        else
+        {
+          power /= 10;
+        }
+      }
+    }
+  }
+  return sign * val / power;
 }
 
 int getLine(char str[], int lim)
