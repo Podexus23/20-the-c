aeb4c8c40ef5eac89f0c735522e1f78c9372eb7c
Podexus23 <santon23@gmail.com>
2024-09-27 10:40:10 +0300

    feat: struct writing

--- basics/st6/st6.c
+++ basics/st6/st6.c
@@ -20,10 +20,100 @@ Create a to-do list application where the user can add, delete, or modify tasks.
 #include <stdlib.h>
 #include <string.h>
 
+struct task
+{
+  int id;
+  char task[50];
+};
+
+int save(char *filename, struct task *p, int n);
+int load(char *filename);
+
 int main(void)
 {
+  char *filename = "task.txt";
+  struct task st6 = {1, "finish the 6-th task"};
+  struct task tasks[] =
+      {
+          {1, "finish the 6-th task\n"},
+          {2, "do something\n"},
+          {3, "finish the 7th task\n"}};
+  int n = sizeof(tasks) / sizeof(tasks[0]);
 
-  FILE *fp = fopen("task.txt", "w");
+  save(filename, tasks, n);
+  load(filename);
+
+  return 0;
+}
+
+int save(char *filename, struct task *p, int n)
+{
+  char *c;
+  int size = n * sizeof(struct task);
+
+  FILE *fp = fopen(filename, "wb");
+
+  if (!fp)
+  {
+    printf("Error occured while oppening\n");
+    return 1;
+  }
+
+  c = (char *)&n;
+  for (int i = 0; i < sizeof(n); i++)
+  {
+    putc(*c++, fp);
+  }
+  c = (char *)p;
+  for (int i = 0; i < size; i++)
+  {
+    putc(*c, fp);
+    c++;
+  }
   fclose(fp);
+  return 0;
+}
+
+int load(char *filename)
+{
+  char *c;
+  int m = sizeof(int);
+  int n;
+  FILE *fp = fopen(filename, "rb");
+
+  if (!fp)
+  {
+    printf("Error occured while opening file\n");
+    return 1;
+  }
+
+  int *ptr_count = malloc(m);
+  c = (char *)ptr_count;
+
+  while (m > 0 && (*c = getc(fp)) != EOF)
+  {
+    c++;
+    m--;
+  }
+
+  n = *ptr_count;
+  free(ptr_count);
+
+  struct task *ptr = malloc(n * sizeof(struct task));
+  c = (char *)ptr;
+
+  while ((*c = getc(fp)) != EOF)
+  {
+    c++;
+  }
+
+  printf("Tasks:\n");
+
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d. %s \n", (ptr + i)->id, (ptr + i)->task);
+  }
+  fclose(fp);
+  free(ptr);
   return 0;
 }
